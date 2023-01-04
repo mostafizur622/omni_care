b1887b3cab823d76c3eff2ad49e75116bf661c3b
Tanvir3488 <Tanvir3488@gmail.com>
2023-01-04 19:26:47 +0600

    office

--- app/src/main/java/com/srapp/Db_Actions/URL.java
+++ app/src/main/java/com/srapp/Db_Actions/URL.java
@@ -24,7 +24,7 @@ public class URL {
     public static final String OUTLET_WISE_SALES_REPORT = Domain + "dist_outlet_wise_sales_report.json";
     public static final String INCENTIVE_PARTY = Domain + "get_bonus_cards_report.json";
     public static final String GIFT_ISSUE = Domain + "giftitem_received.json";
-    public static final String VERSION = "1.3.0";
+    public static final String VERSION = "1.3.1";
     public static final String [] EMAIL = {"tanvir.ahmed@arenaphonebd.net","abu.naser@arenaphonebd.net"};
     public static final String VERSION_txt = "Version(" + VERSION + ")";
     public static String release_date = "03-JAN-2023";
