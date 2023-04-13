b46aa511de50e7b0c33ae081ea39e82f0631bb3d
tanvir3488 <babuand8@gmail.com>
2023-04-13 22:51:42 +0600

    2023-04-11

--- app/src/main/java/com/srapp/Db_Actions/URL.java
+++ app/src/main/java/com/srapp/Db_Actions/URL.java
@@ -23,8 +23,8 @@ public class URL {
   //public static final String Domain = "http://192.168.10.82:90/smc-e-sales/api_data_dist124Retrives/"; //2 may 2021
 
 
-    //public static final String Domain = "http://182.160.103.234:8079/api_data_dist135Retrives/"; //after_marge dec 6 2021
-    public static final String Domain = "http://202.126.123.157/smc_test/api_data_dist135_retrives/"; //after_marge dec 6 2021
+    public static final String Domain = "http://182.160.103.234:8079/api_data_dist135Retrives/"; //after_marge dec 6 2021
+    //public static final String Domain = "http://202.126.123.157/smc_test/api_data_dist135_retrives/"; //after_marge dec 6 2021
    //public static final String Domain = "http://52.148.73.105/api_data_dist123Retrives/"; //after_marge dec 6 2021
    //public static final String Domain = "http://202.126.123.157/smc_test_mk/api_data_dist122Retrives/"; //after_marge dec 6 2021nnnnnnnnn
 
@@ -47,7 +47,7 @@ public class URL {
     public static final String VERSION = "1.3.6";
     public static final String [] EMAIL = {"tanvir.ahmed@arenaphonebd.net","abu.naser@arenaphonebd.net"};
     public static final String VERSION_txt = "Version(" + VERSION + ")";
-    public static String release_date = "04-APRIL-2023";
+    public static String release_date = "13-APRIL-2023";
     public static final String OUTLET_VISIT_REPORT =Domain +"get_route_wise_outlet_visit_report.json";
     public static final String Bonus_Policy = Domain + "get_policy_list_v2.json"; //
     public static final String Bonus_Policy_Outlet = Domain + "get_outlet_group.json"; //
